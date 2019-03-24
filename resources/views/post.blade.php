ab3f1599f420cf392427b9ab947c74c3f923513a
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-03-24 19:00:54 -0300

    reparando estilo en post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,11 +5,11 @@
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="img/favicon.png" type="image/png">
         <title>Laravel</title>
-       <!--  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons">
         <link rel="stylesheet" href="https://unpkg.com/vue-material@beta/dist/theme/default.css">
         <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
         <!-- imports new -->
         <!-- Bootstrap CSS -->
