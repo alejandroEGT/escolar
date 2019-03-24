5e4de11e6c1bcfc669d9c8118b5847fff56de47f
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-03-24 18:11:48 -0300

    quitando un jquery

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-     <!--    <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-        <meta content="width=device-width,initial-scale=1,minimal-ui" name="viewport">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/favicon.png" type="image/png">
         <title>Laravel</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic|Material+Icons">
@@ -13,7 +13,7 @@
 
         <!-- imports new -->
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="css/bootstrap.css">
+        <!-- <link rel="stylesheet" href="css/bootstrap.css"> -->
         <link rel="stylesheet" href="vendors/linericon/style.css">
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
@@ -27,7 +27,7 @@
         <meta name="csrf-token" value="{{ csrf_token() }}" />
     </head>
     <body>
-   
+  
 
         <div id="app">
         
@@ -64,11 +64,8 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos
         </div>
     </footer>
     <!--================End Footer Area =================-->
-
-        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
-
-
-        <script src="js/jquery-3.2.1.min.js"></script>
+    
+       <!--  <script src="js/jquery-3.2.1.min.js"></script> -->
     <script src="js/popper.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/stellar.js"></script>
@@ -83,5 +80,8 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
     <script src="js/gmaps.min.js"></script>
     <script src="js/theme.js"></script>
+        <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+
+
     </body>
 </html>
