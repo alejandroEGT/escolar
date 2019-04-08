5f1729766a052b3b46cf59f825b4401216448c81
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-04-08 11:53:56 -0300

    cambiando query de alumnos

--- app/Alumno_curso.php
+++ app/Alumno_curso.php
@@ -18,18 +18,30 @@ class Alumno_curso extends Model
 		// WHERE `cu`.`cuenta_id` = 3
 
 		if ($curso == '') {
-			$query = DB::select('SELECT * FROM `alumno` as `a` 
-			LEFT join `alumno-curso` as `ac` on `ac`.`alumno_id` = `a`.`id`
-			LEFT join `curso` as `cu` on `cu`.`id` = `ac`.`curso_id`
-			WHERE `cu`.`cuenta_id` = ?', [ $cuenta_id ]);
+			// $query = DB::select('SELECT * FROM alumno as a 
+			// LEFT join 'alumno-curso' as ac on ac.alumno_id = a.id
+			// LEFT join curso as cu on cu.id = ac.curso_id
+			// WHERE cu.cuenta_id = ?', [ $cuenta_id ]);
+
+
+			$query = Alumno::leftjoin('alumno-curso as ac','ac.alumno_id','alumno.id')
+						   ->leftjoin('curso as c','c.id','ac.curso_id')
+						   ->where('c.cuenta_id', $cuenta_id)->get();
 
 			return $query;
 		}
 		if ($curso != '') {
-			$query = DB::select('SELECT * FROM `alumno` as `a` 
-			LEFT join `alumno-curso` as `ac` on `ac`.`alumno_id` = `a`.`id`
-			LEFT join `curso` as `cu` on `cu`.`id` = `ac`.`curso_id`
-			WHERE `cu`.`cuenta_id` = ?  and `ac`.`curso_id` = ?', [ $cuenta_id, $curso ]);
+			// $query = DB::select('SELECT * FROM alumno as a 
+			// LEFT join alumno-curso as ac on ac.alumno_id = a.id
+			// LEFT join curso as cu on cu.id = ac.curso_id
+			// WHERE cu.cuenta_id = ?  and ac.curso_id = ?', [ $cuenta_id, $curso ]);
+
+			$query = Alumno::leftjoin('alumno-curso as ac','ac.alumno_id','alumno.id')
+						   ->leftjoin('curso as c','c.id','ac.curso_id')
+						   ->where([
+						   	'c.cuenta_id'=> $cuenta_id,
+						   	'ac.curso_id'=> $curso
+						   ])->get();
 
 			return $query;
 		}
