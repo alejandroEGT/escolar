16a47c1429c9bcb97ab60da5099eaa5096cb7b59
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-04-23 03:15:15 -0300

    jsonss

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -179,7 +179,7 @@ class DocenteController extends Controller
 
     		 $sum++;
     	}
-    	return response()->json($array_return);
+    	dd(response()->json($array_return));
     }
     public function listar_docentes_colegio()
     {
